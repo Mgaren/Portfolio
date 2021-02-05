b7602c11926a57f09a9f8c396bdd6c574b6c1131
Mgaren <basketeur009@gmail.com>
2021-02-05 10:31:45 +0100

    add projet admin

--- src/Controller/ProjetPersoController.php
+++ src/Controller/ProjetPersoController.php
@@ -6,29 +6,33 @@ use App\Entity\ProjetPerso;
 use App\Form\ProjetPersoType;
 use App\Repository\ProjetPersoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProjetPersoController extends AbstractController
 {
     /**
      * @Route("/projetPerso", name="projetPerso")
+     * @param ProjetPersoRepository $persoRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(ProjetPersoRepository $persoRepository): Response
     {
-        /** @var ProjetPersoRepository $repository */
-        $this->getDoctrine()->getRepository(ProjetPerso::class);
-
-        return $this->render('projetPerso/projetPerso.html.twig');
+        return $this->render('projetPerso/projetPerso.html.twig', [
+            'projetPersos' => $persoRepository->findAll()
+        ]);
     }
 
     /**
      * @Route("/projetPerso/new", name="projetPerso_new", methods={"GET", "POST"})
      * @param Request $request
+     * @param SluggerInterface $slugger
      * @return Response
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $projetPerso = new ProjetPerso();
         $form = $this->createForm(ProjetPersoType::class, $projetPerso);
@@ -36,6 +40,23 @@ class ProjetPersoController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $imageFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeImageFilename = $slugger->slug($imageFilename);
+                $newImageFile = $safeImageFilename . '-' . uniqid('', true) . '-' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('upload_dir'),
+                        $newImageFile
+                    );
+                } catch (FileException $e) {
+                }
+                $projetPerso->setImage($newImageFile);
+            }
             $entityManager->persist($projetPerso);
             $entityManager->flush();
 
